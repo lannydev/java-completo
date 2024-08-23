012994247b9f5c48169e2598dd38bf913a22585a
Elaine <e.cristina00@gmail.com>
2024-08-23 19:20:35 -0300

    Aula de Casting

---
+++ curso-programacao/src/AulaCasting.java
@@ -0,0 +1,30 @@
+
+public class AulaCasting {
+
+	public static void main(String[] args) {
+
+		double B, b, h, area;
+		
+		B = 7.5;
+		b = 5.5;
+		h = 8.2;
+		
+		area = ((B + b) / 2) * h;
+		
+		System.out.println(B);
+		System.out.println(b);
+		System.out.println(h);
+		System.out.println(area);
+		
+		// Exemplo de casting
+		
+		int a = 5;
+		int c = 2;
+		double resultado = (double) a / c;
+		
+		System.out.println(resultado);
+		
+
+	}
+
+}
